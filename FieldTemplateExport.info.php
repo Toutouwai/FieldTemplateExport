69f89c9994f53554a44937d66aded51bce9e9cb4
Robin Sallis <Toutouwai@users.noreply.github.com>
2024-10-13 15:06:20 +1300

    v0.1.2
    Adds button to download export data as json file.

--- FieldTemplateExport.info.php
+++ FieldTemplateExport.info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'title' => 'Field and Template Export',
 	'summary' => 'Get export data from Edit Field and Edit Template.',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'author' => 'Robin Sallis',
 	'href' => 'https://github.com/Toutouwai/FieldTemplateExport',
 	'icon' => 'files-o',
--- FieldTemplateExport.js
+++ FieldTemplateExport.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
+	// Select textarea contents on focus
 	$('.fte-select-on-focus').on('focus', function() {
 		$(this).select();
+	});
+
+	// Download export file when button is clicked
+	$('.fte-export-button').on('click', function() {
+		const name = $(this).data('fte-name');
+		const text = $(this).siblings('.fte-select-on-focus').val();
+		const $link = $(`<a href="data:text/plain;charset=utf-8,${encodeURIComponent(text)}" download="${name}.json" style="display:none"></a>`);
+		$('body').append($link);
+		$link[0].click();
+		$link.remove();
 	});
 
 });
